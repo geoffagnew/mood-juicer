55a4841cf7680637553d561d838ad047aa5416e1
Geoff Agnew <geoff.agnew@utoronto.ca>
2016-09-13 14:12:18 -0400

    Fixed evaluation of two numbers. No videos will be presented in duplicate.

--- js/main.js
+++ js/main.js
@@ -1,63 +1,40 @@
 $(function() {
 
-	// var testGroup = ['blue', 'green', 'yellow'];
-	var videos = ['https://www.youtube.com/embed/Yt6Ig18C8dA=ONE', 'https://www.youtube.com/embed/-F5HwiGm7lg=TWO','https://www.youtube.com/embed/W6p2onuGlpo=THREE'];
-	var previousVid = 'Empty';
-	var rounds = '';
+	var videos = ['https://www.youtube.com/embed/Yt6Ig18C8dA', 'https://www.youtube.com/embed/-F5HwiGm7lg','https://www.youtube.com/embed/W6p2onuGlpo'];
+	var vidNumber = videos.length;
+	var previousVid = '';
+	var currentVid = '';
 
 	$('#boost-btn').on('click', function(e) {
 		e.preventDefault();
-		// print the previous number selected
-		$('#test-block-previous').html('This is the previous number: ' + previousVid);
-		// get the current number selected
-		var currentVid = randomNumber();
-
 		// call function to compare values
-		var vettedVid = numberComparison(currentVid, previousVid);
+		var vettedNum = compareNumbers(previousVid);
+		// get the video url from array
+		var src = videos[vettedNum];
+		// add the video url to the iframe
+		var vidContainer = $('#video-output iframe').attr('src', src);
+		console.log(src);
 
 		// ------------------------------------------------
 		// for testing purposes print the video to the page
-		$('#test-block').html('This is the current url: ' + vettedVid[0]);
-		$('#test-block-current').html('This is the current number selected: ' + vettedVid[1]);
-
-		// for testing purposes, print the rounds to the page
-		rounds += 'Current url: ' + vettedVid[0] + '<br />' +
-					'Previous number: ' + previousVid + '<br />' +
-					'Current number: ' + vettedVid[1] + '<br /><br />';
-		$('#rounds').html(rounds);
+		// $('#test-block').html('This is the current url: ' + src);
+		// // print the previous number selected
+		// $('#test-block-previous').html('This is the previous number: ' + previousVid);
+		// // print the current number
+		// $('#test-block-current').html('This is the current number selected: ' + vettedNum);
 		// -------------------------------------------------
 
 		// finally store the current number for future use in storePrevNumber
 		storePrevNumber(currentVid);
 	});
 
-	// compare current and previous values 
-	function numberComparison(current, previous) {
-		// create local variable for current randomized number
-		var randomized = current;
-		var alertNote = '';
-		// check if current value is equal to previous value
-		// (if current value is the same as previous value) 
-		//	if randomized === previous {
-			// (randomize number) 
-		// }
-
-		while (randomized === previous) {
-		 	alertNote = $('#test-block-alert').html('Oops we have a duplicate. The previous number was: ' + previous + ' and the original randomized number is ' + randomized);
-		 	randomized = randomNumber();
-		}
-		var videoSelect = pickVideo(randomized);
-		var combined = [videoSelect,randomized];
-		return combined;
-		// return pickVideo(randomized);
+	function compareNumbers(previous) {
+  		currentVid = Math.floor(Math.random() * vidNumber);
+  		while (previous === currentVid) {
+   			currentVid = Math.floor(Math.random() * vidNumber);
+  		}
+  		return currentVid;
 	}
-
-	// find a random number
-	function randomNumber() {
- 		var vidNumber = videos.length;
- 		var number = Math.floor(Math.random() * vidNumber);
- 		return number;
- 	}
 
  	// pick a video from the array after ensuring no two videos will be shown consecutively
  	function pickVideo(pos) {
@@ -69,47 +46,5 @@ $(function() {
  	function storePrevNumber(val) {
  		previousVid = val;
  	}
-
- 	// FUNCTION FOR TESTING COMPARISON
- 	// (function () {
-	// 	var randomized = randomNumber();
-	// 	var prev = 5;
-	// 	var i = 0;
-
-	// 	while (randomized === prev) {
-	// 	 	$('#test-block-2').html('Oops we have a duplicate. The previous number was: ' + prev + ' and the original randomized number is ' + randomized);
-	// 	 	randomized = randomNumber();
-	// 	}
-	// 	$('#test-block').html('This is the current number selected: ' + randomized);
-	// })();
-
-
-	// var videos = ['https://www.youtube.com/embed/Yt6Ig18C8dA', 'https://www.youtube.com/embed/-F5HwiGm7lg','https://www.youtube.com/embed/W6p2onuGlpo'];
-
- // 	// Event listener for randomizer
- // 	$('#boost-btn').on('click', function(e) {
- // 		e.preventDefault();
- // 		// var currentVideo = pickVideo();
-	// 	var vidContainer = $('#video-output iframe').attr('src', pickVideo());
-	// 	var src = pickVideo();
-	// 	// setPrevVid(pickVideo());
-	// 	// console.log('This is the current video: ' + currentVideo);
-	// 	// previousVideo = pickVideo();
-	// 	$('#test-block').html(src);
-	// 	// 
- // 	});
-
-
- // 	function randomNumber() {
- // 		var vidNumber = videos.length;
- // 		var number = Math.floor(Math.random() * vidNumber);
- // 		return number;
- // 	}
-
- // 	function pickVideo() {
- // 		var videoSelect = videos[randomNumber()];
- // 		return videoSelect;
- // 	}
-
 
 });
