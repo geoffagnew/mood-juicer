1e326c8dc4e56d9717e21aff54c3f8d92c8e7170
Geoff Agnew <gagnew@gmail.com>
2016-09-14 08:46:54 -0400

    Added logic for switching background colour.

--- js/main.js
+++ js/main.js
@@ -1,19 +1,26 @@
 $(function() {
 
-	var videos = ['https://www.youtube.com/embed/Yt6Ig18C8dA', 'https://www.youtube.com/embed/-F5HwiGm7lg','https://www.youtube.com/embed/W6p2onuGlpo'];
+	var videos = ['https://www.youtube.com/embed/Yt6Ig18C8dA?autoplay=1', 'https://www.youtube.com/embed/-F5HwiGm7lg?autoplay=1','https://www.youtube.com/embed/W6p2onuGlpo?autoplay=1'];
+	var colours = ['red', 'orange', 'green', 'purple'];
 	var vidNumber = videos.length;
+	var colourNumber = colours.length;
 	var previousVid = '';
 	var currentVid = '';
+	var $videoWrap = $('#video-output');
 
 	$('#boost-btn').on('click', function(e) {
 		e.preventDefault();
+		// set the background colour
+		setColour();
+		if ($videoWrap.hasClass('hide')) {
+			$videoWrap.removeClass('hide');
+		}
 		// call function to compare values
 		var vettedNum = compareNumbers(previousVid);
 		// get the video url from array
 		var src = videos[vettedNum];
 		// add the video url to the iframe
-		var vidContainer = $('#video-output iframe').attr('src', src);
-		console.log(src);
+		$('#video-output iframe').attr('src', src);
 
 		// ------------------------------------------------
 		// for testing purposes print the video to the page
@@ -28,10 +35,16 @@ $(function() {
 		storePrevNumber(currentVid);
 	});
 
+	// randomize function for selecting random number from arrays
+	function randomize(val) {
+		var num = Math.floor(Math.random() * val);
+		return num;
+	}
+
 	function compareNumbers(previous) {
-  		currentVid = Math.floor(Math.random() * vidNumber);
+  		currentVid = randomize(vidNumber);
   		while (previous === currentVid) {
-   			currentVid = Math.floor(Math.random() * vidNumber);
+   			currentVid = randomize(vidNumber);
   		}
   		return currentVid;
 	}
@@ -39,6 +52,21 @@ $(function() {
  	// store the value of previous number in the previousVid variable
  	function storePrevNumber(val) {
  		previousVid = val;
+ 	}
+
+ 	// select and set new background colour
+ 	function setColour() {
+ 		var selectColour = randomize(colourNumber);
+ 		var src = colours[selectColour];
+ 		var $body = $('body');
+ 		$body.toggleClass(src);
+ 		var currentClass = $body.attr('class');
+ 		if ($body.hasClass(currentClass)) {
+ 			$body.removeClass(currentClass);
+ 			$body.addClass(src);
+ 		} else {
+ 			$body.addClass(src);
+ 		}
  	}
 
 });
