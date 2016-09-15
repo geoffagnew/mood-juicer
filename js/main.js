32881fedd46d5e74e22b69fa8ac471d4b669279c
Geoff Agnew <geoff.agnew@utoronto.ca>
2016-09-15 08:53:19 -0400

    Trying to fix weird issue with conflicts between branches.

--- js/main.js
+++ js/main.js
@@ -1,14 +1,11 @@
 $(function() {
 
-	// var videos = ['https://www.youtube.com/embed/Yt6Ig18C8dA', 'https://www.youtube.com/embed/-F5HwiGm7lg','https://www.youtube.com/embed/W6p2onuGlpo', 'https://www.youtube.com/embed/U5aqQvEeMro', 'https://www.youtube.com/embed/dzt7oCFN3ek'];
-	// var videosWatched = [];
-	var videos = ['one', 'two', 'three', 'four', 'five'];
-	var videosWatched = [];
+	var videos = ['https://www.youtube.com/embed/Yt6Ig18C8dA', 'https://www.youtube.com/embed/-F5HwiGm7lg','https://www.youtube.com/embed/W6p2onuGlpo', 'https://www.youtube.com/embed/U5aqQvEeMro', 'https://www.youtube.com/embed/dzt7oCFN3ek'];
 	var colours = ['red', 'orange', 'green', 'purple'];
-	// var vidNumber = videos.length;
+	var vidNumber = videos.length;
 	var colourNumber = colours.length;
-	// var previousVid = '';
-	// var currentVid = '';
+	var previousVid = '';
+	var currentVid = '';
 	var $videoWrap = $('#video-output');
 	var $btn = $('#boost-btn');
 
@@ -30,24 +27,24 @@ $(function() {
 			$videoWrap.removeClass('hide');
 		}
 		// call function to compare values
-		// var vettedNum = compareNumbers(previousVid);
+		var vettedNum = compareNumbers(previousVid);
 		// get the video url from array
-		// var src = videos[vettedNum];
-		
-		var videoData = getVideo();
-		var src = videoData[0];
-		console.log('Original videos array: ' + videos);
-
-		console.log('Second videos array: ' + videosWatched);
-
-		console.log('Number of videos left in original array: ' + videoData[1] + '------------');
-
-		// src += '?autoplay=1';
+		var src = videos[vettedNum];
+		src += '?autoplay=1';
 		// add the video url to the iframe
-		// $('#video-output iframe').attr('src', src);
+		$('#video-output iframe').attr('src', src);
+
+		// ------------------------------------------------
+		// for testing purposes print the video to the page
+		// $('#test-block').html('This is the current url: ' + src);
+		// // print the previous number selected
+		// $('#test-block-previous').html('This is the previous number: ' + previousVid);
+		// // print the current number
+		// $('#test-block-current').html('This is the current number selected: ' + vettedNum);
+		// -------------------------------------------------
 
 		// finally store the current number for future use in storePrevNumber
-		// storePrevNumber(currentVid);
+		storePrevNumber(currentVid);
 	});
 
 	// randomize function for selecting random number from arrays
@@ -56,53 +53,18 @@ $(function() {
 		return num;
 	}
 
-	function getVideo() {
-		if (videos.length <= 0) {
-			// move all the items in videosWatched array back to videos array
-			// videos += videosWatched;
-			var vidNumber = videos.length;
-			// set the random number to pull from the array
-			var currentVid2 = randomize(vidNumber);
-			// get the video url based on that number
-			var videoSrc = videos[currentVid2];
-			// remove that item from the videos array
-			videos.splice(currentVid2, 1);
-			// push that url into the videosWatched array
-			videosWatched.push(videoSrc);
-			// get the new number of videos in array
-			vidNumber = videos.length; 
-			videos = videosWatched.slice();
-			videos.pop();
-			videosWatched = [];
-		} else {
-			var vidNumber = videos.length;
-			// set the random number to pull from the array
-			var currentVid2 = randomize(vidNumber);
-			// get the video url based on that number
-			var videoSrc = videos[currentVid2];
-			// remove that item from the videos array
-			videos.splice(currentVid2, 1);
-			// push that url into the videosWatched array
-			videosWatched.push(videoSrc);
-			// get the new number of videos in array
-			vidNumber = videos.length;
-		}
-		
-		return [videoSrc, vidNumber];
+	function compareNumbers(previous) {
+  		currentVid = randomize(vidNumber);
+  		while (previous === currentVid) {
+   			currentVid = randomize(vidNumber);
+  		}
+  		return currentVid;
 	}
 
-	// function compareNumbers(previous) {
- //  		currentVid = randomize(vidNumber);
- //  		while (previous === currentVid) {
- //   			currentVid = randomize(vidNumber);
- //  		}
- //  		return currentVid;
-	// }
-
  	// store the value of previous number in the previousVid variable
- 	// function storePrevNumber(val) {
- 	// 	previousVid = val;
- 	// }
+ 	function storePrevNumber(val) {
+ 		previousVid = val;
+ 	}
 
  	// select and set new background colour
  	function setColour() {
